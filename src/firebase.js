a09d38585d8e57dbe3f73544aadcda1b74c46304
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-06 22:45:37 +0530

    Cleanup and inital setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvi6sSaHifFkd5KPMJdVKZRyjK1I_8POI",
+    authDomain: "clone-b30aa.firebaseapp.com",
+    projectId: "clone-b30aa",
+    storageBucket: "clone-b30aa.appspot.com",
+    messagingSenderId: "291146060958",
+    appId: "1:291146060958:web:955358045c0fcc7bdf578f"
+  };
