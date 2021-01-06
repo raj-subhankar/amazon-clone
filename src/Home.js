053e07acfc57aa38b6be75f564c8cae1b8dd8132
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-07 00:36:32 +0530

    Implement Product

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+import Product from './Product'
+
+function Home() {
+    return(
+        <div className='home'>
+            <div className='home__container'>
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/SingleTitle/Coolie/launch/WP/1500x600_Hero-Tall_WP._CB412534930_.jpg" alt="" />
+            
+                <div className='home__row'>
+                    <Product title="The lean startup" price="630" image="https://m.media-amazon.com/images/I/81jgCiNJPUL._AC_UY327_QL65_.jpg" rating={5}/>
+                    <Product title="Dyson V7 Animal Cord-Free Vacuum (Purple)" price="29,900" image="https://m.media-amazon.com/images/I/51-Xiz66bzL._AC_UY327_QL65_.jpg" rating={4}/>
+                    <Product title="Redmi Note 9 Pro (Glacier White, 4GB RAM, 64GB Storage)" price="12,900" image="https://images-na.ssl-images-amazon.com/images/I/81rpzlanj1L._SL1500_.jpg" rating={4}/>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
