237ce81e01aa063da9cf8273ade71bed575bed34
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-07 12:13:53 +0530

    Implement store

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Product.css'
+import {useStateValue} from './StateProvider'
 
-function Product({ title, image, price, rating }) {
+
+function Product({ id, title, image, price, rating }) {
+    const[{ basket }, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
+
     return(
         <div className='product'>
             <div className='product__info'>
@@ -18,7 +35,7 @@ function Product({ title, image, price, rating }) {
                 </div>
             </div>
             <img src={image} />
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
