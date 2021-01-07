af7f1d6ee50b8acc460c2e92bb4cfa96a986d762
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-07 14:22:09 +0530

    Implement CheckoutProduct

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue()
     return(
         <div className="checkout">
             <div className="checkout__left">
@@ -11,9 +14,16 @@ function Checkout() {
                 src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Laptops/INTEL/LFH_Work--Banner--1500x200.jpg" alt="" />
 
                 <h2 className="checkout__title">Your Shopping Basket</h2>
-            </div>
-            <div className="checkout__right">
 
+                {basket.map(item => (
+                    <CheckoutProduct 
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                    />
+                ))}
             </div>
 
             <div className="checkout__right">
--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ function Home() {
                 <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/SingleTitle/Coolie/launch/WP/1500x600_Hero-Tall_WP._CB412534930_.jpg" alt="" />
             
                 <div className='home__row'>
-                    <Product id="123" title="The lean startup" price="630" image="https://m.media-amazon.com/images/I/81jgCiNJPUL._AC_UY327_QL65_.jpg" rating={5}/>
-                    <Product id="124" title="Dyson V7 Animal Cord-Free Vacuum (Purple)" price="29,900" image="https://m.media-amazon.com/images/I/51-Xiz66bzL._AC_UY327_QL65_.jpg" rating={4}/>
-                    <Product id="125" title="Redmi Note 9 Pro (Glacier White, 4GB RAM, 64GB Storage)" price="12,900" image="https://images-na.ssl-images-amazon.com/images/I/81rpzlanj1L._SL1500_.jpg" rating={4}/>
+                    <Product id="123" title="The lean startup" price={630} image="https://m.media-amazon.com/images/I/81jgCiNJPUL._AC_UY327_QL65_.jpg" rating={5}/>
+                    <Product id="124" title="Dyson V7 Animal Cord-Free Vacuum (Purple)" price={29900} image="https://m.media-amazon.com/images/I/51-Xiz66bzL._AC_UY327_QL65_.jpg" rating={4}/>
+                    <Product id="125" title="Redmi Note 9 Pro (Glacier White, 4GB RAM, 64GB Storage)" price={12900} image="https://images-na.ssl-images-amazon.com/images/I/81rpzlanj1L._SL1500_.jpg" rating={4}/>
 
                 </div>
             </div>
