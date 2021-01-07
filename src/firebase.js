1877de7c51a8647dcf7e3fffda40cecccbf0b2c4
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-07 15:23:02 +0530

    Implement user authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase'
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCvi6sSaHifFkd5KPMJdVKZRyjK1I_8POI",
     authDomain: "clone-b30aa.firebaseapp.com",
@@ -6,3 +9,9 @@ const firebaseConfig = {
     messagingSenderId: "291146060958",
     appId: "1:291146060958:web:955358045c0fcc7bdf578f"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+
+  export { db, auth }
